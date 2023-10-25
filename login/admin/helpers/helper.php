de5a83413d4f5134ac263649e78e3cb75225ecef
VIN <115197877+Binn12345@users.noreply.github.com>
2023-10-25 17:04:16 +0800

    temporary push

--- login/admin/helpers/helper.php
+++ login/admin/helpers/helper.php
@@ -1,19 +1,31 @@
 <?php
 
-    function role($role="")
-    {
-        
-        $return = "";
-        if ($role == "1"){
-            echo '<pre>', print_r($return = "Superadmin", true) ?: 'undefined index', '</pre>';
-        } 
-        if ($role == "2"){
-            echo '<pre>', print_r($return = "Student", true) ?: 'undefined index', '</pre>';
-        } 
-        if ($role == "3"){
-            echo '<pre>', print_r($return = "Admin", true) ?: 'undefined index', '</pre>';
-        } 
+    function role($role = "") {
+
+        $return = [ 
+                  "1" => "Superadmin", 
+                  "2" => "Student", 
+                  "3" => "Admin"
+                ];
+
+        try {
+
+            echo $return[$role] ?? "Undefined index";
+
+        } catch (Exception $e) {
+
+            echo $e->getMessage();
+
+        }
 
     }
+
+   
+
+
+  
+
+
+
 
 
